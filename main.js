986621fce91ff52808e68f869786a10f11ab2f47
jak28 <jak28@sfu.ca>
2024-05-19 11:57:55 -0700

    updated port

--- main.js
+++ main.js
@@ -29,6 +29,6 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(3001, () => {
-  console.log('Server started on port 3001');
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
 });
